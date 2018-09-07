985c3ed51a9b516bca581f9202cd04ac1c60e96f
Neil Patel <neilp980@yahoo.com>
2018-09-07 00:03:54 -0400

    changes to front and backend

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,19 +1,58 @@
 <template>
     <div>
         <form>
-            <input type="text">
-            <input type="submit" value="Search">
+            <input type="text" v-model="message" v-on:input="autocomplete(message)">
+            <input id="searchbar" type="submit" value="Search">
+            <p>{{ songs }}</p>
         </form> 
     </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-
+  data() {
+    return {
+      message: 'hello',
+      songs: []
+    };
+  },
+  methods: {
+        autocomplete: function(input) {
+                        var songs
+                        axios({ method: "GET", url: "http://localhost:3000/search/"+input }).then(
+                             result => {
+                              console.log(result.data);
+                               this.songs = result.data;
+                               console.log(songs);
+                             },
+                            error => {
+                                 console.error(error);
+                            }
+                        )
+                          console.log(songs);
+                        
+                        return songs;
+                        },
+        myFunc: function(input) {
+            return input;
+        }
+            },
+  
+  mounted() {
+    axios({ method: "GET", url: "http://localhost:3000/search/rockstar" }).then(
+      result => {
+          console.log(result);
+        this.songs = result.data;
+        console.log(this.songs);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 };
 </script>
 
 <style>
-
-
 </style>
--- src/server.js
+++ src/server.js
@@ -1,1 +1,46 @@
 var http = require("http");
+
+const express = require('express')
+const app = express();
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: 'ba88dc9350c3444c8928d60d30038157',
+    clientSecret: 'd64dbbee9e6446efbcf5321dfb312678',
+    redirectUri: 'localhost:3000'
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', function (req, res) {
+    res.send('root');
+});
+
+app.get('/search/:song', function (req, res) {
+    spotifyApi.clientCredentialsGrant().then(function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+    
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+    }, function (err) {
+        console.log('Something went wrong when retrieving an access token', err.message);
+    });    
+    spotifyApi.searchTracks(req.params.song).then(function (data) {
+        var songs = [];
+        for (var track of data.body.tracks.items) {
+            songs.push(track.name);
+        }
+        res.send(songs);
+    }, function (err) {
+        res.send(err);
+    });
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
