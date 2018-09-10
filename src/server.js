775ae132083ac9898691a456443abe73077ccb94
Neil Patel <neilp980@yahoo.com>
2018-09-10 19:51:08 -0400

    .

--- src/server.js
+++ src/server.js
@@ -118,4 +118,4 @@ app.get('/song/:songId', function(req, res) {
     });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('server listening on port 3000!'))
