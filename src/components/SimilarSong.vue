884b2af0c341428f725993c77c184d8054aa9250
Neil Patel <neilp980@yahoo.com>
2018-09-16 21:29:55 -0400

    Added similar song

---
+++ src/components/SimilarSong.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="header">
+      
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "albumName", "img", "spotifyUrl", "artist"],
+};
+</script>
+
+<style>
+</style>
